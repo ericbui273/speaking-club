9b3c91254ca9c4008fd4d80b1a9060cc1e1965b4
Eric Bui <buitrung.phuongthao@gmail.com>
2026-09-25 15:55:10 +0300

    Add search function

--- forum.py
+++ forum.py
@@ -1,28 +1,35 @@
 import db
 
-def get_sessions():
-    sql = "SELECT * FROM speaking_session"
+def get_meetups():
+    sql = "SELECT * FROM meetup"
     return db.query(sql)
 
-def get_session(session_id):
+def get_meetup(meetup_id):
     sql = """SELECT s.*, u.username
-            FROM speaking_session AS s, users AS u
-            WHERE s.host_id = u.id AND s.id = ?"""
-    return db.query(sql,[session_id])[0]
+            FROM meetup AS m, users AS u
+            WHERE m.host_id = u.id AND m.id = ?"""
+    return db.query(sql,[meetup_id])[0]
 
-def add_session(title, languages, date_time, venue, avail_slot, content, host_id):
-    sql = """INSERT INTO speaking_session (title, languages, date_time, venue, avail_slot, content, host_id) 
+def add_meetup(title, languages, date_time, venue, avail_slot, content, host_id):
+    sql = """INSERT INTO meetup (title, languages, date_time, venue, avail_slot, content, host_id) 
             VALUES(?,?,?,?,?,?,?)"""
     db.execute(sql,[title, languages, date_time, venue, avail_slot, content, host_id])
-    session_id = db.last_insert_id()
-    return session_id
+    meetup_id = db.last_insert_id()
+    return meetup_id
 
-def edit_session(title, languages, date_time, venue, avail_slot, content, id):
-    sql = """UPDATE speaking_session
+def edit_meetup(title, languages, date_time, venue, avail_slot, content, id):
+    sql = """UPDATE meetup
             SET title = ?, languages = ?, date_time = ?, venue = ?, avail_slot = ?, content = ?
             WHERE id = ?"""
     db.execute(sql,[title, languages, date_time, venue, avail_slot, content, id])
 
-def remove_session(session_id):
-    sql = "DELETE FROM speaking_session WHERE id = ?"
-    db.execute(sql, [session_id])
+def remove_meetup(meetup_id):
+    sql = "DELETE FROM meetup WHERE id = ?"
+    db.execute(sql, [meetup_id])
+
+def search(query):
+    sql = """SELECT s.*, u.username
+            FROM meetup AS m, users AS u
+            WHERE m.host_id = u.id AND (m.title LIKE ? OR m.languages LIKE ? OR venue LIKE ? OR content LIKE ? OR u.username LIKE ?)
+            ORDER BY m.date_time"""
+    return db.query(sql,["%" + query + "%"]*5)
